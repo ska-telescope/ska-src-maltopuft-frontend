ff625f582c0d4138f6d13b7634c78dec049ade33
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-04-19 14:52:06 +0100

    TEAL-539: Add component test coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,11 @@ import coverageTask from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+        exclude: "cypress/**/*.*",
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       coverageTask(on, config);
@@ -10,8 +15,12 @@ export default defineConfig({
   },
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite'
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, config) {
+      coverageTask(on, config);
+      return config;
     }
   }
 });
