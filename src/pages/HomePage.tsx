5fc3c4a6ac6a9dcc860376b8696c11d776d3f17a
Elliot Goodwin <elliot.goodwin@manchester.ac.uk>
2024-09-18 12:43:03 +0100

    DAAC-318: Remove PingButton from / route

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,9 @@
 import LoginButton from '@/features/auth/components/LoginButton';
-import { doPing } from '@/features/health/ping';
 
 function HomePage() {
   return (
     <div>
       <LoginButton />
-      <button type="button" onClick={doPing}>
-        Ping backend
-      </button>
     </div>
   );
 }
